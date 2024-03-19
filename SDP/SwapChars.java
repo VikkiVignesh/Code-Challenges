f56afaf928f7f215a4a0e9defa70d1c4ecae2adc
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-03-19 13:58:42 +0530

    solved 2nd question

---
+++ SDP/SwapChars.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SwapChars {
+    public static void main(String[] args) {
+        String s="we are reva students";
+        char arr[]=s.toCharArray();
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
